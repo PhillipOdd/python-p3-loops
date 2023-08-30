8c4dfbfa0f3993742a19dc24af3538454f1519c7
PhillipOdd <135106680+PhillipOdd@users.noreply.github.com>
2023-08-30 11:52:37 +0300

    Add files via upload

---
+++ lib/testing/looping.py
@@ -0,0 +1,34 @@
+def happy_new_year():
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -=1
+    print("happy new year")
+
+happy_new_year()
+
+def square_integers(int_list):
+    return [num ** 2 for num in int_list]
+
+
+
+
+def fizzbuzz(num):
+    if num % 3 == 0 and num %5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
+    
+def fizzbuzz_printer():
+    for num in range(1, 101):
+        print(fizzbuzz(num))
+
+fizzbuzz_printer()
+    
+
+
+
